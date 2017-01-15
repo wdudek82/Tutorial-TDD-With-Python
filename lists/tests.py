4fba9add0950335af0bce714116f67462565fdae
Wojciech Dudek <wdudek82@gmail.com>
2017-01-15 20:22:18 +0100

    Refactoring home page view to user a remplate

--- lists/tests.py
+++ lists/tests.py
@@ -7,14 +7,18 @@ from .views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolver_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolver_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_corect_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        # request = HttpRequest()
+        # response = home_page(request)
+        response = self.client.get('/')
+
+        # html = response.content.decode('utf-8')
+        # self.assertTrue(html.startswith('<html lang="en">'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.strip().endswith('</html>'))  # Remove any whitespace at the end of the .html file
+
+        self.assertTemplateUsed(response, 'lists/home.html')
